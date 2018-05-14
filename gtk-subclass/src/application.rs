ca4f1c95d32611c7bdbd02cf7a59e23b95191aad
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-05-14 14:47:41 +0200

    add window_added and window_removed virtual functions

--- gtk-subclass/src/application.rs
+++ gtk-subclass/src/application.rs
@@ -20,8 +20,15 @@ use gio_subclass::application::{ApplicationClassExt as GApplicationClassExt,
 
 
 pub trait ApplicationImpl<T: ApplicationBase>:
-    ObjectImpl<T> + GApplicationImpl<T> + AnyImpl + 'static {
+    ObjectImpl<T> + GApplicationImpl<T> + AnyImpl + 'static
+{
+    fn window_added(&self, application: &T, window: &gtk::Window) {
+        application.parent_window_added(window)
+    }
 
+    fn window_removed(&self, application: &T, window: &gtk::Window) {
+        application.parent_window_removed(window)
+    }
 }
 
 pub trait ApplicationImplExt<T> {}
@@ -32,6 +39,27 @@ any_impl!(ApplicationBase, ApplicationImpl);
 pub unsafe trait ApplicationBase:
     IsA<gtk::Application> + IsA<gio::Application> + ObjectType {
 
+    fn parent_window_added(&self, window: &gtk::Window) {
+        unsafe {
+            let klass = self.get_class();
+            let parent_klass = (*klass).get_parent_class() as *const gtk_ffi::GtkApplicationClass;
+            (*parent_klass)
+                .window_added
+                .map(|f| f(self.to_glib_none().0, window.to_glib_none().0))
+                .unwrap_or(())
+        }
+    }
+
+    fn parent_window_removed(&self, window: &gtk::Window) {
+        unsafe {
+            let klass = self.get_class();
+            let parent_klass = (*klass).get_parent_class() as *const gtk_ffi::GtkApplicationClass;
+            (*parent_klass)
+                .window_removed
+                .map(|f| f(self.to_glib_none().0, window.to_glib_none().0))
+                .unwrap_or(())
+        }
+    }
 }
 
 pub unsafe trait ApplicationClassExt<T: ApplicationBase>
@@ -41,7 +69,8 @@ where
     fn override_vfuncs(&mut self, _: &ClassInitToken) {
         unsafe {
             let klass = &mut *(self as *const Self as *mut gtk_ffi::GtkApplicationClass);
-            // klass.render = Some(application_render::<T>);
+            klass.window_added = Some(application_window_added::<T>);
+            klass.window_removed = Some(application_window_removed::<T>);
         }
     }
 }
@@ -74,7 +103,15 @@ macro_rules! box_gtk_application_impl(
 
         impl<T: $crate::application::ApplicationBase> $crate::application::ApplicationImpl<T> for Box<$name<T>>
         {
+            fn window_added(&self, application: &T, window: &gtk::Window){
+                let imp: &$name<T> = self.as_ref();
+                imp.window_added(application, window)
+            }
 
+            fn window_removed(&self, application: &T, window: &gtk::Window){
+                let imp: &$name<T> = self.as_ref();
+                imp.window_removed(application, window)
+            }
         }
     };
 );
@@ -82,7 +119,7 @@ macro_rules! box_gtk_application_impl(
 box_gtk_application_impl!(ApplicationImpl);
 
 impl ObjectType for Application {
-    const NAME: &'static str = "RsApplication";
+    const NAME: &'static str = "RsGtkApplication";
     type ParentType = gtk::Application;
     type ImplType = Box<ApplicationImpl<Self>>;
     type InstanceStructType = InstanceStruct<Self>;
@@ -94,4 +131,35 @@ impl ObjectType for Application {
     }
 
     object_type_fns!();
+}
+
+
+unsafe extern "C" fn application_window_added<T: ApplicationBase>(
+    ptr: *mut gtk_ffi::GtkApplication,
+    window: *mut gtk_ffi::GtkWindow)
+where
+    T::ImplType: ApplicationImpl<T>,
+{
+    callback_guard!();
+    floating_reference_guard!(ptr);
+    let application = &*(ptr as *mut T::InstanceStructType);
+    let wrap: T = from_glib_borrow(ptr as *mut T::InstanceStructType);
+    let imp = application.get_impl();
+
+    imp.window_added(&wrap, &from_glib_borrow(window))
+}
+
+unsafe extern "C" fn application_window_removed<T: ApplicationBase>(
+    ptr: *mut gtk_ffi::GtkApplication,
+    window: *mut gtk_ffi::GtkWindow)
+where
+    T::ImplType: ApplicationImpl<T>,
+{
+    callback_guard!();
+    floating_reference_guard!(ptr);
+    let application = &*(ptr as *mut T::InstanceStructType);
+    let wrap: T = from_glib_borrow(ptr as *mut T::InstanceStructType);
+    let imp = application.get_impl();
+
+    imp.window_removed(&wrap, &from_glib_borrow(window))
 }
